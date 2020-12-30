999e1342b017455b93ffcaae0a0132559c98c2d8
KiharaTakahiro <takahirokihara123@gmail.com>
2020-12-30 15:49:44 +0900

    ある程度の画面を寄せる

--- web/src/componets/templates/register_user_template.tsx
+++ web/src/componets/templates/register_user_template.tsx
@@ -6,12 +6,32 @@ import EMailText from "../parts/text_fields/email_text"
 import AgeText from "../parts/text_fields/age_text"
 import PartsTitle from "../parts/styles/title"
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import { useForm } from 'react-hook-form'
 import ApiCaller from '../../service/api_caller'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 
 type Props = {}
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(2),
+        width: '40ch',
+      },
+      '& .MuiButton-root': {
+        margin: theme.spacing(2),
+        width: '52ch',
+        height: '7ch'
+      },
+
+    },
+  }),
+)
+
 const RegisterUserTemplate: React.FC<Props> = ({}) => {
+  const classes = useStyles()
   const { handleSubmit, register, errors } = useForm()
   const onSubmit = (data: Object) => { 
     console.log(data)
@@ -23,17 +43,24 @@ const RegisterUserTemplate: React.FC<Props> = ({}) => {
       console.log(error)
     })
   }
+
   return (
-    <Grid container direction="column" justify="center" alignItems="center">
-      <PartsTitle title="ユーザ登録画面"/>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <UserNameText required register={register} errors={errors}/>
-        <PasswordText required register={register} errors={errors} />
-        <EMailText required register={register} errors={errors} />
-        <AgeText register={register} errors={errors} />
-        <PrimaryButton button_name="登録"/>
-      </form>
-    </Grid>
+    <>
+      <Grid container direction="column" justify="center" alignItems="center">
+        <PartsTitle title="ユーザ登録画面"/>
+        <Paper  elevation={3}>
+          <form className={classes.root} onSubmit={handleSubmit(onSubmit)}>
+            <Grid container direction="column" justify="center" alignItems="center">
+              <UserNameText required register={register} errors={errors}/>
+              <PasswordText required register={register} errors={errors}/> 
+              <EMailText required register={register} errors={errors} />
+              <AgeText register={register} errors={errors} />
+              <PrimaryButton button_name="登録"/>
+            </Grid>
+          </form>
+        </Paper>
+      </Grid>
+    </>
   )
 };
 
